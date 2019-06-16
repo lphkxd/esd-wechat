c2d9c17e49341ffb2e646580035b0853be424ee5
毛自豪 <836001996@qq.com>
2019-06-16 09:38:18 +0800

    Update start_server.php

--- examples/start_server.php
+++ examples/start_server.php
@@ -7,22 +7,14 @@ use ESD\Plugins\WeChat\WeChatPlugin;
 use ESD\Server\Co\ExampleClass\DefaultServer;
 
 
-function p($val, $title = null, $starttime = '')
+function p($val, $title = null)
 {
     print_r('[ ' . date("Y-m-d H:i:s") . ']:');
     if ($title != null) {
         print_r("[" . $title . "]:");
     }
     print_r($val);
-    /* if ($starttime != '') {
-         $endtime = explode(' ', microtime());
-         $thistime = $endtime[0] + $endtime[1] - ($starttime[0] + $starttime[1]);
-         $thistime = round($thistime, 3);
-         print_r(' -- 执行耗时' . $thistime . '秒。');
-     }
-    print_r("\r\n");*/
     print_r("\r\n");
-
 }
 
 require __DIR__ . '/../vendor/autoload.php';
